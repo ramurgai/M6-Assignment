30ae74a90f4ddfa3f367a0ded2e91c1c9341daa7
Rahul Murgai <ramurgai@my.waketech.edu>
2025-02-13 13:59:10 -0500

    This was a nice and easy assignment. It was actually very fun. More assignments like this please.

--- game.js
+++ game.js
@@ -6,16 +6,25 @@ let userGuess = false;
 let userNum;
 // Create while loop that continues for as long as the user guesses are incorrect.
 while (userGuess === false) {
+    // Loop should count the number of guesses.
+    // Loop should exit if user enters 999 and not display "Too high!"
+    // If the guess is too low, the user should be promped "Too low!" and be able to guess again.
+    // If the guess is too high, the user should be promped 'Too high!" and be able to guess again.
+    // If anything else is entered, the user should be prompted that their input is invalid and 
+    // to guess again.
+
+    // If the guess is correct, the user should be alerted of their win and 
+    // be told the number of guesses they made.
     attempts ++
     userNum = Number(prompt("Enter a number whole number 1-10 inclusive or 999 to end game"))
     if (userNum === 999){
         console.log("The game is over")
-        console.log(`You made ${randomNumber} guesses`)
+        console.log(`You made ${attempts} guesses`)
         break
     }
     else if (userNum === randomNumber){
         console.log("WOW you did it! Good job!")
-        console.log(`You made ${randomNumber} guesses`)
+        console.log(`You made ${attempts} guesses`)
         break
     }
     else if (userNum > randomNumber){
@@ -24,16 +33,7 @@ while (userGuess === false) {
     else if (userNum < randomNumber){
         console.log("Your guess was too low")
     }
-    else{
+    else {
         console.log("You did not enter a number")
     }
 }
-// Loop should count the number of guesses.
-// Loop should exit if user enters 999 and not display "Too high!"
-// If the guess is too low, the user should be promped "Too low!" and be able to guess again.
-// If the guess is too high, the user should be promped 'Too high!" and be able to guess again.
-// If anything else is entered, the user should be prompted that their input is invalid and 
-// to guess again.
-
-// If the guess is correct, the user should be alerted of their win and 
-// be told the number of guesses they made.
